9bc0b4c3768276604034ad508179fb293149a35e
HumzaMKhan <humzamk@uci.edu>
2021-05-23 10:40:27 -0700

    Added Backend code.

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const WebSocApi = require('websoc-api')
+var app = express()
+app.use(express.json())
+
+app.post('/', async (req, res) => {
+    try {
+        const jsonData = await WebSocApi.callWebSocAPI(req.body);
+        res.status(200).send(jsonData)
+    } catch (err) {
+        res.status(500).json({error: err.message})
+    }
+})
+
+app.listen(8081, () =>console.log(`Example app listening at port :8081`))
+
+
+
